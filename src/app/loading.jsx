e868e613f2aea9ba32c96dda678ba5b7731e5f2f
Sebastian <acevedosebastian787b@gmail.com>
2024-04-26 04:36:03 -0300

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <Image
+        className="h-52"
+        src="spinner.svg"
+        alt="loading..."
+        width={50}
+        height={50}
+      />
+    </div>
+  );
+}
